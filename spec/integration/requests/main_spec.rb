261927d423d924b9d83ce2298e5a2928a5c3d131
stanislaw <s.pankevich@gmail.com>
2011-09-25 18:50:34 +0300

    remove junk from spec

--- spec/integration/requests/main_spec.rb
+++ spec/integration/requests/main_spec.rb
@@ -11,22 +11,7 @@ describe "Requests" do
     end
 
     it "should get root" do
-      get '/carrier'
-      response.status.should be(200)
-    end
-
-    it "should get /messages" do
-      get '/carrier/messages'
-      response.status.should be(200)
-    end
-
-    it "should get /sent" do
-      get '/carrier/messages/sent'
-      response.status.should be(200)
-    end
-
-    it "should get /archive" do
-      get '/carrier/messages/archive'
+      get '/'
       response.status.should be(200)
     end
 
