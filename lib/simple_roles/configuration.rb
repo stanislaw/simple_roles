56236e8d226de5ddbdfc4a3e21b9c4a32a92606a
stanislaw <s.pankevich@gmail.com>
2011-10-07 04:48:22 +0300

    nice self extend trick

--- lib/simple_roles/configuration.rb
+++ lib/simple_roles/configuration.rb
@@ -2,31 +2,31 @@ require 'singleton'
 module SimpleRoles
   module Configuration
 
-    class << self
-      attr_accessor :valid_roles, :user_models
+    extend self
+    
+    attr_accessor :valid_roles, :user_models
 
-      def user_models
-        @user_models ||= []
-      end
+    def user_models
+      @user_models ||= []
+    end
 
-      def valid_roles= vr
-        raise "There should be an array of valid roles" if !vr.kind_of?(Array)
-        @valid_roles = vr
-        distribute_methods
-      end
+    def valid_roles= vr
+      raise "There should be an array of valid roles" if !vr.kind_of?(Array)
+      @valid_roles = vr
+      distribute_methods
+    end
 
-      def valid_roles
-        @valid_roles || default_roles
-      end
+    def valid_roles
+      @valid_roles || default_roles
+    end
 
-      def default_roles
-        [:user, :admin]
-      end
+    def default_roles
+      [:user, :admin]
+    end
 
-      def distribute_methods
-        user_models.each do |um|
-          um.register_roles_methods
-        end
+    def distribute_methods
+      user_models.each do |um|
+        um.register_roles_methods
       end
     end
   end
