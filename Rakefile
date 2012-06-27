eed367f950bce257dcf92f88817de8c0acc16164
stanislaw <s.pankevich@gmail.com>
2012-06-27 03:24:42 +0300

    Rakefile

--- Rakefile
+++ Rakefile
@@ -12,6 +12,9 @@ rescue LoadError
   RDoc::Task = Rake::RDocTask
 end
 
+$:.unshift File.expand_path('lib', File.dirname(__FILE__))
+require 'simple_roles'
+
 require 'jeweler'
 Jeweler::Tasks.new do |gem|
   # gem is a Gem::Specification... see http://docs.rubygems.org/read/chapter/20 for more options
--- lib/simple_roles.rb
+++ lib/simple_roles.rb
@@ -1,13 +1,17 @@
-require "sweetloader"
-require "require_all"
+require 'sweetloader'
+require 'require_all'
 
-require "sugar-high/array"
-require "sugar-high/dsl"
+require 'sugar-high/array'
+require 'sugar-high/dsl'
 
-require "simple_roles/engine" if defined?(Rails)
-require "simple_roles/macros"
+require 'active_record'
 
-require_all File.expand_path("../../app", __FILE__)
+require 'simple_roles/version'
+
+require 'simple_roles/engine' if defined?(Rails)
+require 'simple_roles/macros'
+
+require_all File.expand_path('../../app', __FILE__)
 
 module SimpleRoles
   autoload_modules :Configuration, :Packager, :One, :Many
